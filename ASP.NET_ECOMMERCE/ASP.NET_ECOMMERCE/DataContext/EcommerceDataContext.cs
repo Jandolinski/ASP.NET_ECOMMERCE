6df2012d20f91cbead8cfd66cbd93343b07edfbe
Jan Doliński <jan.dolinski89@gmail.com>
2017-05-27 15:25:08 +0200

    init

---
+++ ASP.NET_ECOMMERCE/ASP.NET_ECOMMERCE/DataContext/EcommerceDataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using ASP.NET_ECOMMERCE.Models;
+
+namespace ASP.NET_ECOMMERCE.DataContext
+{
+    public class EcommerceDataContext : DbContext
+    {
+        public EcommerceDataContext() : base("Ecommerce")
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ ASP.NET_ECOMMERCE/ASP.NET_ECOMMERCE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_ECOMMERCE.Startup))]
+namespace ASP.NET_ECOMMERCE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
