340acd85a0e519068bd14d27b92f66c5e86e3200
Jan Doliński <jan.dolinski89@gmail.com>
2017-05-31 18:38:25 +0200

    Added services

---
+++ ASP.NET_ECOMMERCE/ASP.NET_ECOMMERCE/Services/IDataService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ASP.NET_ECOMMERCE.Services
+{
+    interface IDataService<T> : IDisposable
+    {
+        T GetById(int id);
+        void Save(T obj);
+        void Delete(int id);
+        ICollection<T> GetAll();
+    }
+}
