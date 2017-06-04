d05be7f2c94d640f680f6b3fc95bbe069790eccd
Jan Doliński <jan.dolinski89@gmail.com>
2017-06-04 22:21:29 +0200

    Layout - added css,js,img

--- ASP.NET_ECOMMERCE/ASP.NET_ECOMMERCE/App_Start/BundleConfig.cs
+++ ASP.NET_ECOMMERCE/ASP.NET_ECOMMERCE/App_Start/BundleConfig.cs
@@ -13,6 +13,18 @@ namespace ASP.NET_ECOMMERCE
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryflex").Include(
+                "~/Scripts/jquery.flexslider.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                "~/Scripts/main.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/memenu").Include(
+                "~/Scripts/memenu.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/resslides").Include(
+                "~/Scripts/responsiveslides.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
